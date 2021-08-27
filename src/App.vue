d0fea157d06746c8d5b3518f655cdc3eb65eef18
dingding <dingding.huang@belstar.com.cn>
2021-08-27 18:06:48 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    hi
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  created () {
+    axios.get('https://script.google.com/macros/s/AKfycbykQJoo2tIjlegugf6AyBJfQUEjOWeeFtEMl88H59w/dev')
+      .then(function (response) {
+        // handle success
+        console.log(response)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error)
+      })
+      .then(function () {
+        // always executed
+      })
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
