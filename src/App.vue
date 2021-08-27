43637ca7ec5e8d312cc855e1e22621aca6108552
dingding <dingding.huang@belstar.com.cn>
2021-08-27 18:13:47 +0800

    update url

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@ export default {
   components: {
   },
   created () {
-    axios.get('https://script.google.com/macros/s/AKfycbykQJoo2tIjlegugf6AyBJfQUEjOWeeFtEMl88H59w/dev')
-      .then(function (response) {
+    axios.get('https://script.googleusercontent.com/macros/echo?user_content_key=RQa3UV_bp_A7Ot7b2DI8pf30A6arE2OOTUcokNr27xbC3A-qT0h7SkFfNskX3AbNvZZdqKLHCGlBHlGwxlCpatVoL8S1QuYxm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCJAaF2O3DjeG585D8Kdya5ggRYxt5IwwZRB3qdeyKo-1Wiw_r0D52gFfrcngihQDv55RuLsdhyDMU741UnYodE3UJ8JiiR8kA&lib=MlIA8paqZQMOFrrZqMEnbFiq1-vRkhSuq')
+      .then((response) => {
         // handle success
-        console.log(response)
+        console.log(response.data)
       })
       .catch(function (error) {
         // handle error
